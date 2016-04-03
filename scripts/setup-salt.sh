85d35c9cae8feeaa353c77e2d49077a60eee1502
Tairan Wang <tairan.wang@gmail.com>
2016-04-03 22:28:08 +0800

    open firewall

--- scripts/setup-salt.sh
+++ scripts/setup-salt.sh
@@ -5,5 +5,5 @@ yum install -y salt-master python-pygit2
 systemctl enable salt-master
 systemctl start salt-master
 
-firewall-cmd --add-port=4505/tcp --permanent
+firewall-cmd --add-port=4505-4506/tcp --permanent
 firewall-cmd --reload
